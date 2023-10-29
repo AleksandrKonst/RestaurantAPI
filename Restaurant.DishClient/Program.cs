8880bf26b19749b0133c62c19f7e505d609215dc
AleksandrKonst <genkonst5@gmail.com>
2023-10-29 11:32:18 +0300

    add GRPC

---
+++ Restaurant.DishClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using Restaurant.DishServer;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7184");
+var grpcClient = new Disher.DisherClient(channel);
+Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit):");
+while (true) {
+    Console.ReadKey(true);
+    var request = new DishRequest {
+        Code = "four-cheeses",
+        Name = "Four Cheeses",
+        Diameter = 35,
+        Detaills = "моцарелла фирменный соус альфредо"
+    };
+    var reply = grpcClient.GetDish(request);
+    Console.WriteLine($"Price: {reply.Message}");
+}
---
+++ Restaurant.DishServer/Services/DishService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace Restaurant.DishServer.Services;
+
+public class DishService : Disher.DisherBase {
+    private readonly ILogger<DishService> _logger;
+
+    public DishService(ILogger<DishService> logger)
+    {
+        _logger = logger;
+    }
+    
+    public override Task<DishReply> GetDish(DishRequest request, ServerCallContext context) {
+        return Task.FromResult(new DishReply() { Message = $"Answer {request.Code} was send"});
+    }
+}
